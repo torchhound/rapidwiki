35347f13c9a44043d05e292b7708805b0258aec8
torchhound <unknownmasayoshi@gmail.com>
2018-05-25 23:46:27 -0400

    Replaced code for #23

--- views/js/page.js
+++ views/js/page.js
@@ -1,4 +1,37 @@
 $(document).ready(function() {
+  document.title = window.location.pathname.split("/").pop();
+  $.ajax({
+    type: 'GET',
+    url: '/api/view/page/' + window.location.pathname.split("/").pop(),
+    success: function(data) {
+      if (data.empty === "true") {
+        document.title = 'Not Found';
+        $('#edit').hide();
+        $('#history').hide();
+        prepareEditForm(data);
+      } else {
+        $('#edit').show();
+        $('#history').show();
+        prepareEditForm(data);
+      }
+    }
+  })
+});
+
+function prepareEditForm(data) {
+  $('#output').empty().append($.parseHTML(data.html));
+  $('#historyOutput').empty();
+  for (var i = data.diff.length - 1; i >= 0; i--) {
+    $('#historyOutput').append($("<li><div class='row card'><div class='col-md-12 card-body'><b>" + data.diff[i].hash +
+      "</b><p>" + data.diff[i].difference + "</p><p><b>Category: </b>" + data.diff[i].category + "</p><p></p><p><b>Edited: </b>" +
+      data.diff[i].timestamp + "</p></div></div>"));
+  }
+  $('#title').empty().val(data.raw.title);
+  $('#body').empty().val(data.raw.body);
+  $('#category').empty().val(data.raw.category);
+}
+
+$(document).ready(function() {
   var form = $('#editForm');
   form.submit(function(event) {
     event.preventDefault();
