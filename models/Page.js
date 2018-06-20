a915b753a0d8c78a7e7cb03dd78f1b3d66efd647
torchhound <unknownmasayoshi@gmail.com>
2018-06-20 17:48:22 -0400

    Regressing models/Page.js for testing for #23

--- models/Page.js
+++ models/Page.js
@@ -6,30 +6,11 @@ module.exports = function(sequelize, DataTypes) {
       type: Sequelize.STRING,
       unique: true,
       validate: {
-        is: {
-          args: /^[a-z0-9\s]+/gi,
-          msg: 'Titles may only contain alphanumeric characters and white space.'
-        },
-        len: {
-          args: [1, 60],
-          msg: 'Titles must be less than 60 characters.'
-        }
+        is: /^[a-z0-9\s]+/gi
       }
     },
     body: Sequelize.TEXT,
-    category: {
-      type: Sequelize.STRING,
-      validate: {
-        is: {
-          args: /^[a-z0-9\s]+/gi,
-          msg: 'Categories may only contain alphanumeric characters and white space.'
-        },
-        len: {
-          args: [1, 60],
-          msg: 'Categories must be less than 60 characters.'
-        }
-      }
-    },
+    category: Sequelize.TEXT,
     timestamp: Sequelize.TEXT
   });
   return Page;
